937d19f799cf59ee2b1c387dabd4e5408092ddba
muskan <muskan.sh5442@gmail.com>
2024-09-03 08:59:44 +0530

    first commit

---
+++ odd-even-checker.py
@@ -0,0 +1,6 @@
+#write a program to take a number as input and check weather its even or odd (just print if its even)
+num1 = int(input("enter number 1: "))
+if num1%2==0:
+    print("even")
+else:
+    print("odd")
